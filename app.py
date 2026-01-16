ff545195883c9b77968a194872c1eca4acceb3ae
rethishamoothy <rethishamoorthy@gmail.com>
2026-01-16 17:26:26 +0530

    Convert app to Streamlit for online deployment

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
-from flask import Flask, request, jsonify
+# from flask import Flask, request, jsonify
+# import tensorflow as tf
+# import numpy as np
+# from tensorflow.keras.preprocessing import image
+
+# app = Flask(__name__)
+
+# model = tf.keras.models.load_model("tomato_disease_model.keras")
+
+# class_names = [
+#     "Tomato___Early_blight",
+#     "Tomato___Late_blight",
+#     "Tomato___Tomato_Yellow_Leaf_Curl_Virus",
+#     "Tomato___Tomato_mosaic_virus",
+#     "Tomato___healthy"
+# ]
+
+# @app.route("/predict", methods=["POST"])
+# def predict():
+#     file = request.files["file"]
+#     img = image.load_img(file, target_size=(224,224))
+#     img_array = image.img_to_array(img)
+#     img_array = np.expand_dims(img_array, axis=0) / 255.0
+
+#     pred = model.predict(img_array)
+#     result = class_names[np.argmax(pred)]
+
+#     return jsonify({"prediction": result})
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+
+
+import streamlit as st
 import tensorflow as tf
 import numpy as np
-from tensorflow.keras.preprocessing import image
+from PIL import Image
 
-app = Flask(__name__)
-
+# Load model
 model = tf.keras.models.load_model("tomato_disease_model.keras")
 
 class_names = [
@@ -15,17 +48,16 @@ class_names = [
     "Tomato___healthy"
 ]
 
-@app.route("/predict", methods=["POST"])
-def predict():
-    file = request.files["file"]
-    img = image.load_img(file, target_size=(224,224))
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0) / 255.0
+# App UI
+st.title("🍅 Cropzy - Tomato Disease Detection")
+st.write("Upload a tomato leaf image to detect disease")
+
+uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
+
+if uploaded_file:
+    img = Image.open(uploaded_file).convert("RGB").resize((224, 224))
+    img_array = np.array(img)/255.0
+    img_array = np.expand_dims(img_array, axis=0)
 
     pred = model.predict(img_array)
-    result = class_names[np.argmax(pred)]
-
-    return jsonify({"prediction": result})
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    st.success(f"Prediction: {class_names[np.argmax(pred)]}")
